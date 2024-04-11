3bd94401d6663519fb3a2a0ab8591c2c0ef7620e
Walace Rodrigues <walaceclash10@gmail.com>
2024-04-11 10:06:19 -0300

    API whatsapp

--- Menu/js/app.js
+++ Menu/js/app.js
@@ -11,6 +11,8 @@ var MEU_ENDERECO = null
 var VALOR_CARRINHO = 0
 var VALOR_ENTREGA = 5  
 
+var CELULAR_EMPRESA = '5531994132010'
+
 cardapio.eventos = {
 
     init: () => {
@@ -494,6 +496,47 @@ cardapio.metodos = {
         $("#resumoEndereco").html(`${MEU_ENDERECO.endereco}, ${MEU_ENDERECO.numero}, ${MEU_ENDERECO.bairro}`)
 
         $("#cidadeEndereco").html(`${MEU_ENDERECO.cidade}-${MEU_ENDERECO.uf} / ${MEU_ENDERECO.cep} - ${MEU_ENDERECO.complemento}`)
+
+        cardapio.metodos.finalizarPedido()
+
+    },
+
+    // atualiza o link do botao do wpp
+    finalizarPedido: () => {
+
+        if (MEU_CARRINHO.length > 0  && MEU_ENDERECO != null){
+
+            var texto = 'Olá, gostaria de fazer um pedido:';
+            texto += `\n*Itens do pedido:*\n\n\${itens}`;
+            texto += `\n*Endereço de entrega:*`;
+            texto += `\n${MEU_ENDERECO.endereco}, ${MEU_ENDERECO.numero}, ${MEU_ENDERECO.bairro}`;
+            texto += `\n${MEU_ENDERECO.cidade}-${MEU_ENDERECO.uf} / ${MEU_ENDERECO.cep} ${MEU_ENDERECO.complemento}`;
+            texto += `\n\n*Total (com entrega): R$ ${(VALOR_CARRINHO + VALOR_ENTREGA).toFixed(2).replace('.', ',')}*`;
+
+            var itens = ''
+
+            $.each(MEU_CARRINHO, (i, e) => {
+
+                itens += `*${e.qntd}x* ${e.name} ....... R$ ${e.price.toFixed(2).replace('.', ',')} \n`;
+
+                //ultimo item
+                if ((i + 1) == MEU_CARRINHO.length) {
+
+                    texto = texto.replace(/\${itens}/g, itens);
+
+                    // converte a url 
+
+                    let encode = encodeURI(texto)
+
+                    let URL = `https://wa.me/${CELULAR_EMPRESA}?text=${encode}`
+
+                    $("#btnEtapaResumo").attr('href', URL)
+
+                }
+
+            })
+
+        }
 
     },
 
